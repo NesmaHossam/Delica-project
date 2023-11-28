3460191dc32fa371cd0e22cd66f416b52d4af6f0
NesmaHossam <nh3643769@gmail.com>
2023-11-28 23:50:26 +0200

    add all files in my projects

---
+++ html/index.js
@@ -0,0 +1,76 @@
+
+// nav code
+navbar = document.querySelector(".navbar").querySelectorAll("a");
+console.log(navbar)
+
+navbar.forEach(element => {
+    element.addEventListener("click",function(){
+        navbar.forEach(nav=>nav.classList.remove("active"))
+        this.classList.add("active");
+    })
+});
+
+
+
+
+
+//CONTACT US => BUY
+
+function myFunction() {
+    alert("YOU BOOKED A TABLE :) ");
+  }
+
+
+
+
+// our chef
+
+const the_animation = document.querySelectorAll('.animation')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('scroll-animation')
+        }
+            else {
+                entry.target.classList.remove('scroll-animation')
+            }
+        
+    })
+},
+   { threshold: 0.5
+   });
+//
+  for (let i = 0; i < the_animation.length; i++) {
+   const elements = the_animation[i];
+
+    observer.observe(elements);
+  } 
+
+
+
+
+
+
+
+  
+
+//   signin  => in signpage
+
+// const loginText = document.querySelector(".title-text .login");
+// const loginForm = document.querySelector("form.login");
+// const loginBtn = document.querySelector("label.login");
+// const signupBtn = document.querySelector("label.signup");
+// const signupLink = document.querySelector("form .signup-link a");
+// signupBtn.onclick = (()=>{
+//   loginForm.style.marginLeft = "-50%";
+//   loginText.style.marginLeft = "-50%";
+// });
+// loginBtn.onclick = (()=>{
+//   loginForm.style.marginLeft = "0%";
+//   loginText.style.marginLeft = "0%";
+// });
+// signupLink.onclick = (()=>{
+//   signupBtn.click();
+//   return false;
+// });
